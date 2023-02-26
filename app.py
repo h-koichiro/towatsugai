1f6b528688b577916a479c5396846d5df74aac10
h-koichiro <126397974+h-koichiro@users.noreply.github.com>
2023-02-26 23:00:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,49 @@
 from flask import Flask, request, abort
-from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from linebot import (
+　LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+　InvalidSignatureError
+)
+from linebot.models import (
+　MessageEvent, TextMessage, TextSendMessage, ImageMessage, ImageSendMessage
+)
 
 app = Flask(__name__)
 
-ACCESS_TOKEN = "W4yKr2DpVZXhU6rWnXM+Y/fXgHBsYuOCbSEbOPARRQxp+Ke9A4jyuuYh4kkIDiRj8zFzeZNSg3UDLHVBKqj4gRsMw/ZHH1q4YP0W1IKX8PpagZ6L3gTjApD9jDvuUlUnUodpzECNCdAKmEbRMWJUKQdB04t89/1O/w1cDnyilFU="
-SECRET = "644c830373e830f0a682507bc73b23e6"
+ACCESS_TOKEN = "YOURE_CHANNEL_ACCESS_TOKEN"
+SECRET = "YOUR_CHANNEL_SECRET"
+
+FQDN = "Herokuで割り当てられたURL"
 
 line_bot_api = LineBotApi(ACCESS_TOKEN)
 handler = WebhookHandler(SECRET)
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    signature = request.headers['X-Line-Signature']
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
+　signature = request.headers['X-Line-Signature'] 　body = request.get_data(as_text=True)
+　app.logger.info("Request body: " + body)
 
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    line_bot_api.reply_message(event.reply_token,TextSendMessage(text=event.message.text))
+　try:
+　　handler.handle(body, signature)
+　except InvalidSignatureError:
+　　abort(400)
+
+　return 'OK'
+
+@handler.add(MessageEvent, message=ImageMessage)
+def handle_image_message(event):
+　message_content = line_bot_api.get_message_content(event.message.id)
+　
+　with open("static/" + event.message.id + ".jpg", "wb") as f:
+　　f.write(message_content.content)
+　　line_bot_api.reply_message(
+　　　event.reply_token,
+　　　ImageSendMessage(
+　　　　original_content_url=FQDN + "/static/" + event.message.id + ".jpg",
+　　　　preview_image_url=FQDN + "/static/" + event.message.id + "jpg"
+　　　)
+　　)
 
 if __name__ == "__main__":
-    app.run()
+　app.run()
